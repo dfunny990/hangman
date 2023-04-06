7e0babf68bd141108d89c416795fada9c279310e
Patrick Saperstein <patricksaperstein@gmail.com>
2023-04-06 12:12:00 -0400

    Fix input to console.log hangman word on Submit

--- src/components/WordInput.js
+++ src/components/WordInput.js
@@ -1,19 +1,33 @@
 
 import React from 'react'
 
-function onWordSubmit(hangmanWord) {
-    console.log(hangmanWord);
+function onWordSubmit(e) {
+        // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // You can pass formData as a fetch body directly:
+    // fetch('/some-api', { method: form.method, body: formData });
+
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson.myInput);
 }
 
 const WordInput = () => {
-    const [hangmanWord, setHangmanWord] = React.useState('');
+    // const [hangmanWord, setHangmanWord] = React.useState('');
 
     return (
         <div>
-            <form onSubmit={onWordSubmit(hangmanWord)}>        <label>
-                    Word:
-                <input type="text" value={hangmanWord} onChange={setHangmanWord}/>        </label>
-                <input type="submit" value="Submit" />
+            <form method="post" onSubmit={onWordSubmit}>
+                <label>
+                    Hangman Word: <input name="myInput" defaultValue="Some initial value" />
+                </label>
+
+                <button type="submit">Submit form</button>
             </form>
         </div>
         
